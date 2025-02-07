9b42d35a34ae4ff6ca2f989fd0d9630c26ce8cac
joabesnts09 <joabesilva1233@gmail.com>
2025-02-07 19:17:28 -0300

    fix: responsive design in presentation component

--- src/components/presentation/index.tsx
+++ src/components/presentation/index.tsx
@@ -10,7 +10,7 @@ export const PresentationUnion = () => {
         <div className='w-full flex lg:items-center lg:justify-center'>
           <div className={`flex flex-col gap-4 mb-6 lg:w-[754px] lg:pb-[32px] lg:pt-[199px] lg-plus:pb-[130px] `}>
             <div>
-              <h1 className='w-[383px] text-white text-xl font-bold md:text-[42px] md:w-[550px] md:leading-relaxed'>
+              <h1 className='w-full max-w-[383px] text-white text-xl font-bold md:text-[42px] md:w-[550px] md:max-w-[550px] md:leading-relaxed'>
                 Ganhe{' '}
                 <span className='bg-gradient-to-r from-[#7D208E] to-[#0A45F6] bg-clip-text text-transparent'>
                   experiência
@@ -19,20 +19,20 @@ export const PresentationUnion = () => {
               </h1>
             </div>
 
-            <p className='w-[321px] text-base text-gray-200 leading-[1.5] md:text-2xl md:w-[650px] lg-w-full md:tracking-tight md:leading-[2.3rem]'>
+            <p className='w-full max-w-[321px] text-base text-gray-200 leading-[1.5] md:text-2xl  md:max-w-[650px] lg-w-full md:tracking-tight md:leading-[2.3rem]'>
               Descubra como o Union oferece um ambiente prático e imersivo para
               desenvolvedores juniors ganharem experiência de mercado, mesmo sem
               estar em uma empresa. Participe dessa experiência única e
               impulsione sua carreira como desenvolvedor!
             </p>
 
-            <a className='w-[343px] h-[69px] mt-4 p-0 lg:w-[404px]' href='#'>
+            <a className='w-full max-w-[343px] sm-min:w-[343px] h-[69px] mt-4 p-0 lg:w-[404px]' href='#'>
               <Button className='text-[22px]' text='Quero participar' />
             </a>
           </div>
 
           <div className='hidden lg:flex animate-move-up-down'>
-            <Image className='max-w-[520px]' src={curvedRender} alt='Curved Render' />
+            <Image className='max-w-[520px] lg:w-[400px]' src={curvedRender} alt='Curved Render' />
           </div>
         </div>
       </section>
