47aea54de42de6c72ceae2703f7b0149757ba7a7
joabesnts09 <joabesilva1233@gmail.com>
2025-02-10 16:33:09 -0300

    fix: border gradeient in input label

--- src/components/ui/inputLabel/index.tsx
+++ src/components/ui/inputLabel/index.tsx
@@ -5,7 +5,12 @@ interface IInputProps {
   msgError?: string
   type?: string
 }
-export const InputLabel = ({ name, placeholder, msgError, type='text' }: IInputProps) => {
+export const InputLabel = ({
+  name,
+  placeholder,
+  msgError,
+  type = 'text',
+}: IInputProps) => {
   return (
     <>
       <div className='w-full flex flex-col gap-3 md:gap-5 items-start'>
@@ -14,19 +19,16 @@ export const InputLabel = ({ name, placeholder, msgError, type='text' }: IInputP
         </label>
 
         <input
-          className='w-full pl-4 pr-3 py-3 border-[1px] border-[rgb(189, 189, 189)] bg-clip-padding rounded-[10px] bg-transparent focus:outline-none focus:ring-2 focus:ring-transparent focus:ring-offset-2'
+          className='w-full pl-4 pr-3 py-3 border-[1px] border-[rgb(189, 189, 189)] bg-clip-padding rounded-[10px] bg-transparent focus:outline-none focus:border-transparent focus:ring-2 focus:ring-offset-2 focus:ring-transparent focus:ring-offset-transparent'
           type={type}
           placeholder={placeholder}
+          style={{
+            backgroundImage:
+              'linear-gradient(#1F1D2B, #1F1D2B), linear-gradient(to right, #7D208E, #0A45F6)',
+            backgroundOrigin: 'border-box',
+            backgroundClip: 'padding-box, border-box',
+          }}
         />
-
-        <style jsx>{`
-          input:focus {
-            background-image: linear-gradient(#1f1d2b, #1f1d2b),
-              linear-gradient(to right, #7d208e, #0a45f6);
-            background-origin: border-box;
-            background-clip: padding-box, border-box;
-          }
-        `}</style>
 
         <span className='text-sm text-red-500 hidden'>{msgError}</span>
       </div>
