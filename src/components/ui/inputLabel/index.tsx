171ab8082f7c8e84e7271f835f3945e2f41892fd
joabesnts09 <joabesilva1233@gmail.com>
2025-02-10 19:11:26 -0300

    fix: add props in props

--- src/components/ui/inputLabel/index.tsx
+++ src/components/ui/inputLabel/index.tsx
@@ -10,6 +10,7 @@ export const InputLabel = ({
   placeholder,
   msgError,
   type = 'text',
+  ...props
 }: IInputProps) => {
   return (
     <>
@@ -22,6 +23,7 @@ export const InputLabel = ({
           className='w-full pl-4 pr-3 py-3 border-[1px] border-[rgb(189, 189, 189)] bg-clip-padding rounded-[10px] bg-transparent focus:outline-none focus:border-transparent focus:ring-2 focus:ring-offset-2 focus:ring-transparent focus:ring-offset-transparent'
           type={type}
           placeholder={placeholder}
+          {...props}
           style={{
             backgroundImage:
               'linear-gradient(#1F1D2B, #1F1D2B), linear-gradient(to right, #7D208E, #0A45F6)',
