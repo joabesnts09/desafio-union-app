29e6a372cb28787ecbb59f19ea9ce042215a864b
joabesnts09 <joabesilva1233@gmail.com>
2025-02-10 22:08:05 -0300

    feat: add button reverse

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -6,7 +6,12 @@ interface IButtonProps {
   className?: string
 }
 
-export const Button = ({ text, onClick, className, ...props }: IButtonProps) => {
+export const Button = ({
+  text,
+  onClick,
+  className,
+  ...props
+}: IButtonProps) => {
   return (
     <button
       onClick={onClick}
@@ -21,6 +26,39 @@ export const Button = ({ text, onClick, className, ...props }: IButtonProps) => 
       <span className='relative z-10'>{text}</span>
 
       <div className='absolute inset-0 bg-gradient-to-r from-[#7D208E] to-[#0A45F6] transform origin-right transition-transform duration-500 ease-in-out lg:group-hover:scale-x-0'></div>
+
+      <div
+        className='absolute inset-0 pointer-events-none rounded-sm'
+        style={{
+          border: '2px solid transparent',
+          borderImage:
+            'linear-gradient(225deg, rgb(0,72,254) 0%, rgb(133,29,134) 100%) 1',
+        }}
+      ></div>
+    </button>
+  )
+}
+
+export const ButtonReverse = ({
+  text,
+  onClick,
+  className,
+  ...props
+}: IButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`${className} relative group w-full h-full text-white px-4 py-2 rounded-sm font-semibold overflow-hidden border-2 margin-[-2px]`}
+      {...props}
+      style={{
+        background: 'transparent',
+        border: '2px solid transparent',
+        borderRadius: '8px',
+      }}
+    >
+      <span className='relative z-10'>{text}</span>
+
+      <div className='absolute inset-0 bg-gradient-to-r from-[#7D208E] to-[#0A45F6] transform origin-right scale-x-0 lg:group-hover:scale-x-100 transition-all duration-500 ease-in-out'></div>
 
       <div
         className='absolute inset-0 pointer-events-none rounded-sm'
